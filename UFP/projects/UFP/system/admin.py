595c0d79e3a8d7f1fa986e4c300fca5804677e05
ansley <estevesjancen388@gmail.com>
2025-08-04 19:50:38 +0800

    add the warehouse models on the admin panel

--- UFP/projects/UFP/system/admin.py
+++ UFP/projects/UFP/system/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Service, Department, Sentiment, Program, Student, StudentFeedback, StudentActivityLog, Teacher, TeacherEvaluation   
+from warehouse.models import DimService, DimSentiment, DimStudent, dim_teacher, fact_teacher_evaluation, FactFeedback
+
 # Register your models here.
 admin.site.register(Service)
 admin.site.register(Department)
@@ -9,6 +11,14 @@ admin.site.register(Student)
 admin.site.register(StudentFeedback)
 admin.site.register(Teacher)
 admin.site.register(TeacherEvaluation)
+
+# models from warehouse
+admin.site.register(DimService)
+admin.site.register(DimSentiment)
+admin.site.register(DimStudent)
+admin.site.register(dim_teacher)
+admin.site.register(fact_teacher_evaluation)
+admin.site.register(FactFeedback)
 
 @admin.register(StudentActivityLog)
 class StudentActivityLogAdmin(admin.ModelAdmin):
