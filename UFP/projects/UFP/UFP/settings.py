c2eaa796be757495b0e3a6db2404135ce7000880
ansley <estevesjancen388@gmail.com>
2025-08-04 18:52:18 +0800

    added warehouse models

--- UFP/projects/UFP/UFP/settings.py
+++ UFP/projects/UFP/UFP/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django_recaptcha',
     'rest_framework',
     'api',
+    'warehouse.apps.WarehouseConfig',
 ]
 
 MIDDLEWARE = [
--- UFP/projects/UFP/warehouse/models.py
+++ UFP/projects/UFP/warehouse/models.py
@@ -1,3 +1,84 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class DimStudent(models.Model):
+    student_id = models.CharField(max_length=50, primary_key=True)
+    student_name = models.CharField(max_length=100, blank=True, null=True)
+    program_id = models.IntegerField(blank=True, null=True)
+    program_name = models.CharField(max_length=100, blank=True, null=True)  
+    department_id = models.IntegerField(blank=True, null=True)
+    department_name = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        db_table = 'warehouse"."dim_student'  # Required if schema is warehouse
+
+    def __str__(self):
+        return self.student_name or self.student_id
+
+
+class DimService(models.Model):
+    service_id = models.IntegerField(primary_key=True)
+    service_name = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        db_table = 'warehouse"."dim_service'
+
+    def __str__(self):
+        return self.service_name
+
+
+class DimSentiment(models.Model):
+    sentiment_id = models.IntegerField(primary_key=True)
+    label = models.CharField(max_length=20, blank=True, null=True)
+
+    class Meta:
+        db_table = 'warehouse"."dim_sentiment'
+
+    def __str__(self):
+        return self.label
+
+
+class FactFeedback(models.Model):
+    feedback_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    student = models.ForeignKey(DimStudent, on_delete=models.SET_NULL, null=True, db_column='student_id')
+    service = models.ForeignKey(DimService, on_delete=models.SET_NULL, null=True, db_column='service_id')
+    sentiment = models.ForeignKey(DimSentiment, on_delete=models.SET_NULL, null=True, db_column='sentiment_id')
+    timestamp = models.DateTimeField()
+    comment_length = models.IntegerField(blank=True, null=True)
+    comments = models.TextField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'warehouse"."fact_feedback'
+
+    def __str__(self):
+        return str(self.feedback_id)
+
+class dim_teacher(models.Model):
+    teacher_id = models.CharField(max_length=50, primary_key=True)
+    teacher_name = models.CharField(max_length=100, blank=True, null=True)
+    department_id = models.IntegerField(blank=True, null=True)
+    department_name = models.CharField(max_length=100, blank=True, null=True)
+    program_id = models.IntegerField(blank=True, null=True)
+    program_name = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        db_table = 'warehouse"."dim_teacher'
+
+    def __str__(self):
+        return self.teacher_name or self.teacher_id
+    
+class fact_teacher_evaluation(models.Model):
+    evaluation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    teacher = models.ForeignKey(dim_teacher, on_delete=models.SET_NULL, null=True, db_column='teacher_id')
+    student = models.ForeignKey(DimStudent, on_delete=models.SET_NULL, null=True, db_column='student_id')
+    service = models.ForeignKey(DimService, on_delete=models.SET_NULL, null=True, db_column='service_id')
+    sentiment = models.ForeignKey(DimSentiment, on_delete=models.SET_NULL, null=True, db_column='sentiment_id')
+    timestamp = models.DateTimeField()
+    comment_length = models.IntegerField(blank=True, null=True)
+    comments = models.TextField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'warehouse"."fact_teacher_evaluation'
+
+    def __str__(self):
+        return str(self.evaluation_id) + " - " + (self.teacher.teacher_name if self.teacher else "Unknown Teacher")
