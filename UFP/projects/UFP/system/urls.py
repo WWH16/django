44096bdb0edf1601df6c5562f9d252d58f17a93c
ansley <estevesjancen388@gmail.com>
2025-08-17 10:47:26 +0800

    refactor/system app (views & urls.py)

--- UFP/projects/UFP/system/urls.py
+++ UFP/projects/UFP/system/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.sample, name='sample'),
+    path('', views.site, name='site'),
     # Feedback/CRUD URLs moved to crud app
 ]
 
