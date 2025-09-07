fd32ec028010de85587d64bb33622f60c4063f95
John Ansley Rocel DO <vm_email@example.com>
2025-09-07 04:29:11 +0000

    update dataabse config

--- UFP/projects/UFP/UFP/settings.py
+++ UFP/projects/UFP/UFP/settings.py
@@ -106,14 +106,13 @@ WSGI_APPLICATION = 'UFP.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'universityFeedbackPlatform',
-        'USER': 'postgres',
+        'NAME': 'ufp_db',
+        'USER': 'ufp_user',
         'PASSWORD': 'Estevesjancen06-16-23',
-        'HOST': 'localhost',
+        'HOST': 'localhost',   # <-- important
         'PORT': '5432',
     }
 }
-
 
 
 # Password validation
