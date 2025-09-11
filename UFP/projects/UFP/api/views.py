d45a3346cbcd95abca5ae53078b5c12f757f9f22
worldwidehandsome <149799520+WWH16@users.noreply.github.com>
2025-09-11 22:59:23 +0800

    fix api & add comment tooltip and restrict (#41)

--- UFP/projects/UFP/api/views.py
+++ UFP/projects/UFP/api/views.py
@@ -627,7 +627,7 @@ def service_feedback_by_semester(request, semester_slug=None):
         "program": None,
         "services": results
     })
-
+'''
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
@@ -637,7 +637,7 @@ from openai import OpenAI
 client = OpenAI(api_key="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")
 
 # Test call
-'''response = client.chat.completions.create(
+response = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[
         {"role": "system", "content": "You are a grammar corrector."},
