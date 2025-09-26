65445f590bad8da9bd51ca1110230f45ea255cf4
John Ansley Rocel <estevesjancen388@gmail.com>
2025-09-26 23:00:33 +0800

    update

--- UFP/projects/UFP/UFP/celery.py
+++ UFP/projects/UFP/UFP/celery.py
@@ -1,17 +1,13 @@
+from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-# Set default Django settings
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "UFP.settings")
+# set Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UFP.settings')
 
-# Create Celery app
-app = Celery("UFP")
-
-# Load settings from Django settings (CELERY_ prefix)
-app.config_from_object("django.conf:settings", namespace="CELERY")
-
-# Auto-discover tasks in all installed apps
-app.autodiscover_tasks(lambda: os.environ.get("DJANGO_SETTINGS_MODULE"))
+app = Celery('UFP')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
 
 # Optional debug task
 @app.task(bind=True)
